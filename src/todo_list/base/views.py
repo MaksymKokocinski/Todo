1b55f6c83b18b7cd45a9b5a1f34d47fb8bcf9643
Maksym Kokocinski <m.kokocinski5050@gmail.com>
2021-09-16 15:37:39 +0200

    Adding add/edit/del
    Adding add/edit/delete functionality

--- src/todo_list/base/views.py
+++ src/todo_list/base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
+
 from .models import Task
 
 class TaskList(ListView):
@@ -12,3 +16,17 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = 'base/task_detail.html'
 
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
